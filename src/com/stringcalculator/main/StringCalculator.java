8254c1f7e12e888c838bf4aff98d91e62b37aa9e
Ayush Gupta <ayushgupta028@gmail.com>
2020-10-28 23:08:27 +0530

    First condition is satisfied with empty string,single value and two values.

--- src/com/stringcalculator/main/StringCalculator.java
+++ src/com/stringcalculator/main/StringCalculator.java
@@ -4,12 +4,19 @@ public class StringCalculator {
 
 	public int Add(String numbers) {
 		// TODO Auto-generated method stub
+		String[] arrNum = numbers.split(",");
 		if(numbers.isEmpty()||numbers.equals(""))
-		{return 0;
-	}
-		else
+		{
+			return 0;
+		}
+		else if(numbers.length()==1)
 		{
 			return Integer.parseInt(numbers);
+		}
+		else 
+		{
+			int sum = Integer.parseInt(arrNum[0])+Integer.parseInt(arrNum[1]);
+			return sum;
 		}
 	}
 
